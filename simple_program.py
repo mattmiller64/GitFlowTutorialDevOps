410b7ba4c306a953d45a3874a70906bd75d50420
Matt <miller.matthew64@gmail.com>
2023-01-20 05:49:00 +0000

    Created simple program

---
+++ simple_program.py
@@ -0,0 +1,11 @@
+"""Self contained program meant to add two numbers together"""
+# Class - please fix the following program to correctly add two numbers together
+# Store input numbers
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+# Add two numbers
+result = float(num1) - float(num2)
+
+# Display the sum
+print(f'The sum of {num1} and {num2} is {result}')
